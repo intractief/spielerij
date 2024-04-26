696b6bbebbbdedcbec34fefcd9f299de3f667ca2
Tubby <git@tubby.nl>
2024-04-26 12:47:30 +0200

    implemented berekenScore en allPossibleScoresWithOneDart

--- src/main/java/nl/intractief/spielerij/dart/DartScore.java
+++ src/main/java/nl/intractief/spielerij/dart/DartScore.java
@@ -10,12 +10,16 @@ record DartScore(int score, ScoreType type) {
         List<DartScore> result = new ArrayList<>();
         for(int i=1;i<=20;i++) {
             result.add(new DartScore(i, ScoreType.SINGLE));
+            result.add(new DartScore(i, ScoreType.DOUBLE));
+            result.add(new DartScore(i, ScoreType.TRIPLE));
             //TODO: not complete
         }
+        result.add(new DartScore(25,ScoreType.DOUBLE ));
+        result.add(new DartScore(25,ScoreType.SINGLE));
         return result;
     }
 
     int berekenScore() {
-        return score();
+        return score()* type().multiplier();
     }
 }
